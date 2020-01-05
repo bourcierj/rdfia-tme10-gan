30684aea9c6ca9d6b2c0aac17e9017f929b9ec1a
bourcierj <julesbourcier1@gmail.com>
2020-01-05 18:01:12 +0100

    Update download scripts

--- download_celeba32.sh
+++ download_celeba32.sh
@@ -2,10 +2,10 @@
 
 # Download CelebA dataset with 32x32 images
 
-if ! [ -d "data/celeba/img_align_celeba" ] ; then
-    mkdir -p data/celeba
-    cd data/celeba
-    wget http://webia.lip6.fr/~robert/cours/rdfia/celeba.zip
-    unzip celeba.zip
-    rm -r celeba.zip
+if ! [ -d "data/celeba32/img_align_celeba" ] ; then
+    mkdir -p data/celeba32
+    cd data/celeba32
+    wget http://webia.lip6.fr/~robert/cours/rdfia/celeba32.zip
+    unzip celeba32.zip
+    rm -r celeba32.zip
 fi
--- download_celeba64.sh
+++ download_celeba64.sh
@@ -2,9 +2,9 @@
 
 # Download CelebA dataset with 64x64 images
 
-if ! [ -d "data/celeba/img_align_celeba64" ] ; then
-    mkdir -p data/celeba
-    cd data/celeba
+if ! [ -d "data/celeba64/img_align_celeba" ] ; then
+    mkdir -p data/celeba64
+    cd data/celeba64
     wget http://webia.lip6.fr/~robert/cours/rdfia/celeba64.zip
     unzip celeba64.zip
     rm -r celeba64.zip
