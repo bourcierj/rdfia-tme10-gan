1e5ba93ec9e5619ee3e7d4a63426bd85547dcb43
bourcierj <julesbourcier1@gmail.com>
2020-01-05 14:06:12 +0100

    Add DCGANs modules for 64x64 images
    Plus class renames and argument renames

--- gans.py
+++ gans.py
@@ -2,15 +2,16 @@ import torch
 import torch.nn as nn
 
 
-class Discriminator(nn.Module):
+class Discriminator32(nn.Module):
     """
-    Discriminator network.
+    Discriminator network for 32x32 input images.
     The architecture is inspired from DCGANs (Radford et al 2016).
     """
-    def __init__(self, num_channels_in=32):
-        super(Discriminator, self).__init__()
-        self.num_channels_in = num_channels_in
-        ndf = num_channels_in
+    def __init__(self, num_feature_maps_in=32):
+        super(Discriminator32, self).__init__()
+        self.num_feature_maps_in = num_feature_maps_in
+        ndf = num_feature_maps_in
+        # define layers
         self.model = nn.Sequential(
             nn.Conv2d(3, ndf, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(ndf),
@@ -29,18 +30,84 @@ class Discriminator(nn.Module):
         return self.model(input)
 
 
-class Generator(nn.Module):
+class Generator32(nn.Module):
     """
     Generator network.
     The architecture is inspired from DCGANs (Radford et al 2016)
     """
-    def __init__(self, latent_dim=100, num_channels_out=32):
-        super(Generator, self).__init__()
+    def __init__(self, latent_dim=100, num_feature_maps_out=32):
+        super(Generator32, self).__init__()
         self.latent_dim = latent_dim
-        self.num_channels_out = num_channels_out
-        ngf = num_channels_out
+        self.num_feature_maps_out = num_feature_maps_out
+        ngf = num_feature_maps_out
+        # build a sequential of layers
         self.model = nn.Sequential(
             nn.ConvTranspose2d(latent_dim, 4*ngf, 4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(4*ngf),
+            nn.ReLU(),
+            nn.ConvTranspose2d(4*ngf, 2*ngf, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(2*ngf),
+            nn.ReLU(),
+            nn.ConvTranspose2d(2*ngf, ngf, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(),
+            nn.ConvTranspose2d(ngf, 3, 4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        return self.model(input)
+
+
+class Discriminator64(nn.Module):
+    """
+    Discriminator network for 64x64 input images
+    The architecture is inspired from DCGANs (Radford et al 2016).
+    """
+    def __init__(self, num_feature_maps_in=32):
+        super(Discriminator64, self).__init__()
+        self.num_feature_maps_in = num_feature_maps_in
+        ndf = num_feature_maps_in
+        # build a sequential of layers
+        self.model = nn.Sequential(
+            nn.Conv2d(3, ndf, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(ndf, 2*ndf, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(2*ndf),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(2*ndf, 4*ndf, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(4*ndf),
+            nn.LeakyReLU(0.2),
+            # extra final block here compared to 32x32 inputs
+            nn.Conv2d(4*ndf, 8*ndf, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(8*ndf),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(8*ndf, 1, 4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.model(input)
+
+
+class Generator64(nn.Module):
+    """
+    Generator network.
+    The architecture is inspired from DCGANs (Radford et al 2016)
+    """
+    def __init__(self, latent_dim=100, num_feature_maps_out=32):
+        super(Generator64, self).__init__()
+        self.latent_dim = latent_dim
+        self.num_feature_maps_out = num_feature_maps_out
+        ngf = num_feature_maps_out
+        # build a sequential of layers
+        self.model = nn.Sequential(
+            # extra initial block here compared to 32x32 inputs
+            nn.ConvTranspose2d(latent_dim, 8*ngf, 4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(8*ngf),
+            nn.ReLU(),
+            nn.ConvTranspose2d(8*ngf, 4*ngf, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(4*ngf),
             nn.ReLU(),
             nn.ConvTranspose2d(4*ngf, 2*ngf, 4, stride=2, padding=1, bias=False),
@@ -73,8 +140,8 @@ if __name__ == '__main__':
     num_feature_maps_G = 32
     num_feature_maps_D = 32
 
-    net_G = Generator(latent_dim, num_feature_maps_G)
-    net_D = Discriminator(num_feature_maps_D)
+    net_G = Generator32(latent_dim, num_feature_maps_G)
+    net_D = Discriminator32(num_feature_maps_D)
     # initialize the weights of the networks
     net_G.apply(weights_init)
     net_D.apply(weights_init)
@@ -83,6 +150,22 @@ if __name__ == '__main__':
     z = torch.zeros(16, latent_dim, 1, 1)
     x = torch.zeros(16, 3, 32, 32)
     assert(tuple(net_G(z).size()) == (16, 3, 32, 32))
+    assert(tuple(net_D(x).size()) == (16, 1, 1, 1))
+
+    assert(tuple(net_D(net_G(z)).size()) == (16, 1, 1, 1))
+
+
+    net_G = Generator64(latent_dim, num_feature_maps_G)
+    net_D = Discriminator64(num_feature_maps_D)
+    # initialize the weights of the networks
+    net_G.apply(weights_init)
+    net_D.apply(weights_init)
+
+    # Test the models to check if they have the right shape
+    z = torch.zeros(16, latent_dim, 1, 1)
+    x = torch.zeros(16, 3, 64, 64)
+
+    assert(tuple(net_G(z).size()) == (16, 3, 64, 64))
     assert(tuple(net_D(x).size()) == (16, 1, 1, 1))
 
     assert(tuple(net_D(net_G(z)).size()) == (16, 1, 1, 1))
